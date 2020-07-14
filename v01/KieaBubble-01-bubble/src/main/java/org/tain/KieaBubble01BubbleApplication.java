c84aa40aee381f68e6f8bd1f7c30fc5cdb03e4fe
grtlinux <grtlinux@gmail.com>
2020-07-14 18:58:59 +0900

    up first

---
+++ v01/KieaBubble-01-bubble/src/main/java/org/tain/KieaBubble01BubbleApplication.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaBubble01BubbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaBubble01BubbleApplication.class, args);
+	}
+
+}
